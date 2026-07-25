038c59e8cfff24f9918f05a3665be402d2c45612
Frank Showalter <842058+fshowalter@users.noreply.github.com>
2026-07-25 17:45:25 -0400

    Export `parseFrontmatter` from package entrypoints

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,3 +1,4 @@
+export { parseFrontmatter } from "./src/parseFrontmatter.js";
 export { renderExcerpt } from "./src/renderExcerpt.js";
 export { renderInlineHtml } from "./src/renderInlineHtml.js";
 export { renderMarkdown } from "./src/renderMarkdown.js";
